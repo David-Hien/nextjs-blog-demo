4d8a80f3a9e7ce048340daeca167c11869a88ba5
David Trinh <2023.hien.minh.trinh@uwcisak.jp>
2022-06-11 16:35:25 +0700

    change aaa

--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,13 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Create Next App  1</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">Next.js 11!</a>
         </h1>
 
         <p className="description">
